138fe7a41b544cff053c8a6d78153049a0c08811
melisci <melisascime@gmail.com>
2021-12-08 20:11:01 -0300

    initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require ("express");
+const app = express ();
+const path = require ("path");
+app.set("port", 3000);
+app.listen (app.get("port"), ()=> {
+    console.log ("Servidor corriendo")
+});
+
+
+app.get ("/",(req,res)=>{
+    res.sendFile (path.resolve(__dirname,"./views", "home.html"));
+})
+app.get ("/home",(req,res)=>{
+    res.sendFile (path.resolve(__dirname,"./views", "home.html"));
+})
+app.get ("/register",(req,res)=>{
+    res.sendFile (path.resolve(__dirname,"./views", "register.html"));
+})
+
+app.get ("/login",(req,res)=>{
+    res.sendFile (path.resolve(__dirname,"./views", "login.html"));
+})
+   
+app.use(express.static (path.resolve (__dirname,"./public")))
